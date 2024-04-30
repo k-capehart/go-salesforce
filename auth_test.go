9f17e599828319a50ef9a7477654c45f26177b12
Kyle Capehart (Huntersville) <kyleacapehart@gmail.com>
2024-04-30 15:45:18 -0400

    issue7: extra tests for auth.go

--- auth_test.go
+++ auth_test.go
@@ -23,7 +23,7 @@ func TestValidateAuthFail(t *testing.T) {
 	}
 }
 
-func TestLoginPassword(t *testing.T) {
+func TestInitUsernamePasswordSuccess(t *testing.T) {
 	resp := auth{
 		AccessToken: "1234",
 		InstanceUrl: "example.com",
@@ -38,16 +38,44 @@ func TestLoginPassword(t *testing.T) {
 	}))
 	defer server.Close()
 
-	auth, err := loginPassword(server.URL, "u", "p", "t", "key", "secret")
+	sf, err := Init(Creds{
+		Domain:         server.URL,
+		Username:       "u",
+		Password:       "p",
+		SecurityToken:  "t",
+		ConsumerKey:    "key",
+		ConsumerSecret: "secret",
+	})
 	if err != nil {
-		t.Errorf("expected a successful login, got: %s", err.Error())
+		t.Errorf("unexpected error during salesforce login: %s", err.Error())
 	}
-	if auth.AccessToken != resp.AccessToken ||
-		auth.InstanceUrl != resp.InstanceUrl ||
-		auth.Id != resp.Id ||
-		auth.IssuedAt != resp.IssuedAt ||
-		auth.Signature != resp.Signature {
+	if sf.auth.AccessToken != resp.AccessToken ||
+		sf.auth.InstanceUrl != resp.InstanceUrl ||
+		sf.auth.Id != resp.Id ||
+		sf.auth.IssuedAt != resp.IssuedAt ||
+		sf.auth.Signature != resp.Signature {
 
-		t.Errorf("expected response to be unmarshalled into auth reference, got: %v", auth)
+		t.Errorf("expected response to be unmarshalled into auth reference, got: %v", sf.auth)
+	}
+}
+
+func TestInitUsernamePasswordFail(t *testing.T) {
+	resp := auth{
+		AccessToken: "1234",
+		InstanceUrl: "example.com",
+		Id:          "123abc",
+		IssuedAt:    "01/01/1970",
+		Signature:   "signed",
+	}
+	body, _ := json.Marshal(resp)
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write(body)
+	}))
+	defer server.Close()
+
+	_, err := Init(Creds{})
+	if err == nil {
+		t.Errorf("expected an error during login when no credentials are given")
 	}
 }
